31cae51bfa4fbcba6a5791dd1c145e6e20d00b6d
Diego Hernandez <Diego.Hernandez@ibm.com>
2019-04-30 13:06:05 -0500

    feat(*): introduce plex

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,12 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 const baseFontSize = 14;
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600');
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
 
   body {
-    font-family: 'IBM Plex Sans', Helvertica, Arial, sans-serif;
+    font-family: 'IBM Plex Sans', Helvetica, sans-serif;
     font-size: ${baseFontSize / 16}em;
     line-height: 1.2;
   }
