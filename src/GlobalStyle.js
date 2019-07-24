9bb1767a81da3d69f61e8246793f447bb4cf4803
Diego Hernandez <Diego.Hernandez@ibm.com>
2019-07-24 18:26:06 -0500

    refactor(slides remove them):

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,14 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 const baseFontSize = 14;
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600');
-
   *, *::before, *::after {
     box-sizing: border-box;
   }
 
   body {
-    font-family: 'IBM Plex Sans', Helvetica, sans-serif;
+    font-family: 'Avenir Next', Helvetica, sans-serif;
     font-size: ${baseFontSize / 16}em;
     line-height: 1.2;
   }
